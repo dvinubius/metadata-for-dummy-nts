5538e39d243b8b6dcef9b13379aed294f387cf5f
dvinubius <dvinubiu.narmon@gmail.com>
2024-02-16 21:26:18 +0200

    update images

--- app/api/token/[id]/route.ts
+++ app/api/token/[id]/route.ts
@@ -3,15 +3,15 @@ import { NextResponse } from "next/server";
 const IMAGES = [
   {
     id: 0,
-    url: "https://videos.ctfassets.net/gjyjx7gst9lo/7Bd17vpEKx7ftNsc5QfD1b/644e37f0b12bc27a8f87a39125eee285/MM_01__2_.mp4"
+    url: "https://images.ctfassets.net/gjyjx7gst9lo/17JXIUZvQlh8r1Ms5Txhld/97cc131641d762fb27e6ff721e4fe409/state-of-staking.png?fm=avif&q=100&w=650"
   },
   {
     id: 1,
-    url: "https://videos.ctfassets.net/gjyjx7gst9lo/2ZMlrExSdKTRKMLjFO8V8O/1e5d4fc1850f7f2932c4009915b13ade/MMDEV_01.mp4"
+    url: "https://www.infura.io/_next/image?url=https%3A%2F%2Finfura.ghost.io%2Fcontent%2Fimages%2F2023%2F12%2FUnlock-Simplicity-and-Security-with-Infura-Unified-Key-Experience_1200x628_text.png&w=3840&q=75"
   },
   {
     id: 2,
-    url: "https://videos.ctfassets.net/gjyjx7gst9lo/6aOfXrgejtRg7fZdPepkJN/818428765f2932c5fb1587fbcb097713/Linea_01__1_.mp4"
+    url: "https://www.infura.io/_next/image?url=https%3A%2F%2Finfura.ghost.io%2Fcontent%2Fimages%2F2023%2F11%2FMastering-the-Gas-API-A-Guide-for-Developers_1200x628_text.png&w=3840&q=75"
   },
   {
     id: 3,
@@ -19,15 +19,15 @@ const IMAGES = [
   },
   {
     id: 4,
-    url: "https://videos.ctfassets.net/gjyjx7gst9lo/6JYWUxs5Z140XeNpC9KngE/9591f2bb909506b5f7b4cab034d8b6a6/MM_02__1_.mp4"
+    url: "https://www.infura.io/_next/image?url=https%3A%2F%2Finfura.ghost.io%2Fcontent%2Fimages%2F2023%2F11%2Fimage1.png&w=3840&q=75"
   },
   {
     id: 5,
-    url: "https://videos.ctfassets.net/gjyjx7gst9lo/7A59wssQrNAohCvLkIZlRs/ffe63e793a0f380138cdf4af66071bc4/MM_03__3_.mp4"
+    url: "https://images.ctfassets.net/gjyjx7gst9lo/2djbpxDbGBZIesyMDjqaAV/20c1f5274023a812b887985fc51233d3/why-codefi-1.png?fm=webp&q=85"
   },
   { 
     id: 6,
-    url: "https://videos.ctfassets.net/gjyjx7gst9lo/1F80KvnLLi6YMAoqQSQC10/31033e103c924f2f06a8de1555a35fba/MMDEV_02.mp4"
+    url: "https://images.ctfassets.net/9sy2a0egs6zh/5XRhOQHkOJNsBU8z6O8Jbv/546cc006d9b0dd1064e1aefd36ad1034/Built_for_Developers_Image.png?w=948&h=545&q=80&fm=webp"
   },
   {
     id: 7,
@@ -35,11 +35,11 @@ const IMAGES = [
   },
   {
     id: 8,
-    url: "https://videos.ctfassets.net/gjyjx7gst9lo/lwZ3TLLpLsjLdNhpZVRGr/b69d039b66e2f7067ac5d6fbf0b0cbe2/Infura_01__1_.mp4"
+    url: "https://images.ctfassets.net/gjyjx7gst9lo/75ltnLFXkl5Z6eaaWhgjTZ/ca4d4d45f96f59a921afa5b8987b4df6/launch-mm-staking.png?fm=avif&q=100&w=650"
   },
   {
     id: 9,
-    url: "https://images.ctfassets.net/gjyjx7gst9lo/4dchcVrXP4V9bFActogfll/f6d128cb008197b10eb73020ce01e9b2/Meet-the-Decentralized-Infrastructure-Network-Partners-1x1.png?fm=avif&q=100&w=650"
+    url: "https://images.ctfassets.net/gjyjx7gst9lo/5nQwydGoYf7sBcIfzsTExR/42fd96a42ff2a2d99c6c1c69ddf5590b/Shanghai_Campaign-1x1_2x.png?fm=avif&q=100&w=650"
   }
 ];
 
