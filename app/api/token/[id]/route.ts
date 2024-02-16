dc14ad1008694a59f0ae270e29429aabfb257c1d
dvinubius <dvinubiu.narmon@gmail.com>
2024-02-17 00:53:44 +0200

    update metadata

--- app/api/token/[id]/route.ts
+++ app/api/token/[id]/route.ts
@@ -7,35 +7,35 @@ const IMAGES = [
   },
   {
     id: 1,
-    url: "https://dummynfts.vercel.app/infura1.webp"
-  },
-  {
-    id: 2,
-    url: "https://dummynfts.vercel.app/infura2.webp"
-  },
-  {
-    id: 3,
     url: "https://images.ctfassets.net/gjyjx7gst9lo/6Nk8Yn8W18SM3w5C5URS9E/8486407f435417223b89d52b17922c7a/Brand_Evolution-1x1_2x.png?fm=avif&q=100&w=650"
   },
   {
-    id: 4,
-    url: "https://dummynfts.vercel.app/infura3.webp"
-  },
-  {
-    id: 5,
+    id: 2,
     url: "https://images.ctfassets.net/gjyjx7gst9lo/2djbpxDbGBZIesyMDjqaAV/20c1f5274023a812b887985fc51233d3/why-codefi-1.png?fm=webp&q=85"
   },
   { 
-    id: 6,
+    id: 3,
     url: "https://images.ctfassets.net/9sy2a0egs6zh/5XRhOQHkOJNsBU8z6O8Jbv/546cc006d9b0dd1064e1aefd36ad1034/Built_for_Developers_Image.png?w=948&h=545&q=80&fm=webp"
   },
   {
-    id: 7,
+    id: 4,
     url: "https://images.ctfassets.net/gjyjx7gst9lo/5jvatLAMOoIkbmbUPZJd6s/54a3ffec933bc6818c2d56fbe7e60c80/How_Linea_Works_to_Build_the_World_s_Most_Secure_L2_1x1.png?fm=avif&q=100&w=650"
   },
   {
-    id: 8,
+    id: 5,
     url: "https://images.ctfassets.net/gjyjx7gst9lo/75ltnLFXkl5Z6eaaWhgjTZ/ca4d4d45f96f59a921afa5b8987b4df6/launch-mm-staking.png?fm=avif&q=100&w=650"
+  },
+  {
+    id: 6,
+    url: "https://dummynfts.vercel.app/infura1.webp"
+  },
+  {
+    id: 7,
+    url: "https://dummynfts.vercel.app/infura2.webp"
+  },
+  {
+    id: 8,
+    url: "https://dummynfts.vercel.app/infura3.webp"
   },
   {
     id: 9,
